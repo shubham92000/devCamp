0eb559998763798b782f338f2758d5266198ce86
shubham92000 <sbhsingh616@gmail.com>
2021-10-09 08:06:00 +0530

    added db.json

--- server.js
+++ server.js
@@ -2,11 +2,16 @@ const express = require('express');
 const dotenv = require('dotenv');
 // const logger = require('./middleware/logger');
 const morgan = require('morgan');
-// route files
-const bootcamps = require('./routes/bootcamps');
+const connectDB = require('./config/db');
 
 // load env vars
 dotenv.config({ path:'./config/config.env' });
+
+//connect to database
+connectDB();
+
+// route files
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
@@ -21,4 +26,11 @@ app.use('/api/v1/bootcamps' , bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT , () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT , () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+
+// handle unhandled promise rejections
+process.on('unhandledRejection' , (err , promise) => {
+  console.log(`Error : ${err.message}`);
+  // close server and exit process
+  server.close(() => process.exit(1));
+})
