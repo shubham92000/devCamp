ae79748c4c9845415f8c9b28a32b0f5b5e7dddf3
shubham92000 <sbhsingh616@gmail.com>
2021-10-09 08:24:03 +0530

    added colors

--- config/db.js
+++ config/db.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
+// const colors = require('colors');
 
 const connectDB = async () => {
   const conn = await mongoose.connect(process.env.MONGO_URI , {
     useNewUrlParser : true
   });
-  console.log(`mongo db connected: ${conn.connection.host}`);
+  console.log(`mongo db connected: ${conn.connection.host}`.cyan.underline.bold);
 }
 
 module.exports = connectDB;
