5e288069328bdd1d4dbd370d0d0739a420d4dddf
shubham92000 <sbhsingh616@gmail.com>
2021-10-13 16:07:41 +0530

    added user model , auth routes(login , register)

--- controllers/courses.js
+++ controllers/courses.js
@@ -27,8 +27,8 @@ exports.getCourses = asyncHandler (async (req,res,next) => {
 // @access    Public
 exports.getCourse = asyncHandler (async (req,res,next) => {
   const course = await Course.findById(req.params.id).populate({
-    path : 'bootcamp' ,
-    select : 'name description'
+    path : 'bootcamp' ,           // -> populate which field of courses
+    select : 'name description'   // -> populate what fields of Bootcamp
   });
 
   if(!course){
