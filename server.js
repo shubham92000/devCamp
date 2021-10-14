5a512b5a045c2714caa196ed7c84e95b5ceee390
shubham92000 <sbhsingh616@gmail.com>
2021-10-14 22:39:12 +0530

    added protected routes

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 // const logger = require('./middleware/logger');
 const morgan = require('morgan');
 const fileupload = require('express-fileupload');
+const cookieParser = require('cookie-parser');
 const colors = require('colors');
 const connectDB = require('./config/db');
 const errorHandler = require('./middleware/error');
@@ -24,11 +25,21 @@ const app = express();
 //body parser
 app.use(express.json());
 
+// cookie parser
+app.use(cookieParser());
+
 // app.use(logger);
 // dev logging middleware
 if(process.env.NODE_ENV === 'development'){
   app.use(morgan('dev'));
 }
+
+
+// mount routers
+app.use('/api/v1/bootcamps' , bootcamps);
+app.use('/api/v1/courses' , courses);
+app.use('/api/v1/auth' , auth);
+app.use(errorHandler);
 
 // file uploading
 app.use(fileupload());
@@ -36,11 +47,6 @@ app.use(fileupload());
 // set static folder
 app.use(express.static(path.join(__dirname,'public')));
 
-// mount routers
-app.use('/api/v1/bootcamps' , bootcamps);
-app.use('/api/v1/courses' , courses);
-app.use('/api/v1/auth' , auth);
-app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
